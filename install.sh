32849535f2fc7a6ae2adace3291768773c05e4be
Tamir Sharif <30700375+TamirSharif@users.noreply.github.com>
2023-10-02 13:47:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-apt install gcc
-apt install make
-apt install doxygen
-apt install git
-apt install texlive-latexlbase
-apt install textlive-fonts-recomened textlive-fonts-extra textlive-latex-extra
-apt install plantuml
-apt install graphviz
+apt install gcc -y
+apt install make -y
+apt install doxygen -y
+apt install git -y
+apt install texlive-latexlbase -y
+apt install textlive-fonts-recomened textlive-fonts-extra textlive-latex-extra -y
+apt install plantuml -y
+apt install graphviz -y
